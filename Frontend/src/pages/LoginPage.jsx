560d089fa3a1156773d2d9b04b37bf14b0abeceb
Onkar Godase <onkarlgodase225@gmail.com>
2025-11-22 19:00:55 +0530

    Initial commit

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -87,7 +87,7 @@ export const LoginPage = () => {
             className="w-full py-2 px-4 mt-2 text-sm font-medium text-blue-500 bg-blue-50 dark:bg-gray-700 dark:text-blue-300 rounded-md hover:bg-blue-100 dark:hover:bg-gray-600 transition"
             onClick={() => {
               setValue("emailId", "demo@codemaster.com");
-              setValue("password", "DemoUser123");
+              setValue("password", "DemoUser@123");
             }}
           >
             🔥 Use Demo Login
